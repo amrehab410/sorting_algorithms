00f04040acd0864fe044d05b3ddb82d13f1498b8
root <root@DESKTOP-1N3TEFC>
2024-02-18 01:44:29 +0200

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,6 +4,7 @@
  * bubble_sort - sort list with bubble
  * @array: The array to be printed
  * @size: Number of elements in @array
+ * Return: None
  */
 void bubble_sort(int *array, size_t size)
 {
