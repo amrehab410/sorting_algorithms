0523b8d75fda6f389cedb58ca5f382017d41c361
root <root@DESKTOP-1N3TEFC>
2024-02-18 21:56:21 +0200

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,7 +11,7 @@ void selection_sort(int *array, size_t size)
 	size_t i = 0, j = 0, index = 0;
 	int min = 0;
 
-	if (array == NULL || size < 2)
+	if (array == NULL || size == 0)
 		return;
 
 	for (i = 0; i < size - 1; i++)
