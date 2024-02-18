3ff3c45d81cae09a4c79f843bc4d618a491ea6a5
root <root@DESKTOP-1N3TEFC>
2024-02-18 20:21:55 +0200

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,7 +4,26 @@
  * selection_sort - sort list with selection_sort
  * @array: The array to be printed
  * @size: Number of elements in @array
+ * Return: None
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i = 0, j = 0, index = 0;
+	int min = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array[i];
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < min)
+			{
+				min = array[j];
+				index = j;
+			}
+		}
+		array[index] = array[i];
+		array[i] = min;
+		print_array(array, size);
+	}
 }
